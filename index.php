e4bbb5e11abd585ed936aa6aa6aca8e0b0bb7496
Anthony <anthonymiller@outlook.com>
2013-05-02 15:03:32 -0700

    Initial commit with jquery and bootstrap

---
+++ index.php
@@ -0,0 +1,8 @@
+!DOCTYPE HTML
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Video Game Database</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+
+	</head>
