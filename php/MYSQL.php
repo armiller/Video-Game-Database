682c76fd01dd287b06258899a9a4507ad2f80263
Anthony <anthonymiller@outlook.com>
2013-06-03 02:03:19 -0700

    Added pagenation

--- php/MYSQL.php
+++ php/MYSQL.php
@@ -7,7 +7,6 @@
         $query_result = mysql_query($query);
 
         if (!$query_result) {
-
             die('Invalid query : '.$query. '<br>' . mysqli_error($mysql_handle));
         }
 
@@ -19,11 +18,12 @@
 
     function view_all_video_games() {
 
-        $query = "SELECT video_game.name, esbr.rating, game_studio.name FROM video_game 
-            JOIN esbr ON video_game.esbr_eid = esbr.eid 
-            JOIN video_game_has_game_studio ON video_game_has_game_studio.video_game_gid = video_game.gid 
-            JOIN game_studio ON video_game_has_game_studio.game_studio_sid = game_studio.sid 
-            ORDER BY video_game.name";
+        $query = "SELECT DISTINCT vg.name, e.rating, gs.name, rs_vg.rating FROM video_game vg
+            JOIN esbr e ON vg.esbr_eid = e.eid 
+            JOIN video_game_has_game_studio vs_gs ON vs_gs.video_game_gid = vg.gid 
+            JOIN game_studio gs ON vs_gs.game_studio_sid = gs.sid
+            LEFT OUTER JOIN review_site_has_video_game rs_vg ON rs_vg.video_game_gid = vg.gid
+            ORDER BY vg.name";
 
         $result = db_query($query);
 
@@ -33,9 +33,9 @@
             echo "<td>".$record[0]."</td>";
             echo "<td>".$record[1]."</td>";
             echo "<td>".$record[2]."</td>";
+            echo "<td>".$record[3]."</td>";
             echo "</tr>\n";
         }
-
 
         close_db();
     }
--- videogame.php
+++ videogame.php
@@ -47,16 +47,44 @@
     </div>
 
     <div class="container">
-      <table>
+      <div class="row">
+        <div class="span3">
+          <button class="btn btn-danger">Delete Entry</button>
+        </div>
+      </div>
+
+      <table class="tablesorter">
         <center><h2>Video Games</h2></center>
         <thead>
           <tr>
             <th>Name</th>
             <th>Rating</th>
             <th>Game Studio</th>
+            <th>Review Score</th>
           </tr>
         </thead>
-        <tbody>
+        <tfoot>
+          <tr>
+            <th>Name</th>
+            <th>Rating</th>
+            <th>Game Studio</th>
+            <th>Review Score</th>
+          </tr>
+          <tr>
+            <th colspan="7" class="pager form-horizontal">
+              <button type="button" class="btn first"><i class="icon-step-backward"></i></button>
+              <button type="button" class="btn prev"><i class="icon-arrow-left"></i></button>
+              <span class="pagedisplay"></span> <!-- this can be any element, including an input -->
+              <button type="button" class="btn next"><i class="icon-arrow-right"></i></button>
+              <button type="button" class="btn last"><i class="icon-step-forward"></i></button>
+              <select class="pagesize input-mini" title="Select page size">
+                <option selected="selected" value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+                <option value="40">40</option>
+              </select>
+              <select class="pagenum input-mini" title="Select page number"></select>
+              <tbody>
         <?php 
             ini_set('display_errors',1); 
             error_reporting(E_ALL);
@@ -65,6 +93,8 @@
         ?>
         </tbody>
       </table>
+      <div id="pager" class="pager">
+          <form>
     </div>
 
     <!-- Le javascript
