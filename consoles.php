f4cb890bc113fbc552610b85f905cbe72a686c96
Anthony <anthonymiller@outlook.com>
2013-06-04 13:07:24 -0700

    Added coming soon

--- consoles.php
+++ consoles.php
@@ -42,8 +42,8 @@
 
     <div class="container">
 
-      <h1>Bootstrap starter template</h1>
-      <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
+      <h1>Coming Soon</h1>
+      
 
     </div> <!-- /container -->
 
